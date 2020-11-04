f740b5ec30ea54fe804d251d4d3a48ee8444f85e
{name} <{nickstoia1@ou.edu}>
2020-11-04 17:13:29 -0600

    Person with name

---
+++ src/Person.java
@@ -0,0 +1,20 @@
+
+public class Person implements Comparable<Person>{
+
+	private String name;
+	
+	Person(String name){
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public int compareTo(Person o) {
+		return name.compareTo(o.getName());
+	}
+	
+	
+}
